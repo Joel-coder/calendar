5a98ee900faac044f123332400b871a0d1950f91
Joel Vargas <joelvargasapolinario@gmail.com>
2021-12-20 01:49:17 -0500

    Matrix week per days of the week

--- src/Calendar.js
+++ src/Calendar.js
@@ -2,9 +2,30 @@ import logo from "./logo.svg";
 import moment from "moment";
 import "./App.css";
 
-export default function App() {
+export default function Calendar() {
   const value = moment();
   const startDay = value.clone().startOf("month").startOf("week");
   const endDay = value.clone().endOf("month").endOf("week");
-  return <div>{endDay.format("MM/DD")}</div>;
+  const day = startDay.clone().subtract(1, "day");
+  const calendar = [];
+
+  while (day.isBefore(endDay, "day")) {
+    calendar.push(
+      Array(7)
+        .fill(0)
+        .map(() => day.add(1, "day").clone())
+    );
+  }
+  console.log(calendar);
+  return (
+    <div className="calendar">
+      {calendar.map((week) => (
+        <div>
+          {week.map((day) => (
+            <div className="day">{day.format("D")}</div>
+          ))}
+        </div>
+      ))}
+    </div>
+  );
 }
